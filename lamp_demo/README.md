584884fdc58bd4ae962e917cb173f723b72d79ae
buhahemal <buhahemal10@gmail.com>
2021-03-19 15:57:38 +0000

    first commit

---
+++ lamp_demo/README.md
@@ -0,0 +1,4 @@
+# lamp_demo
+
+TODO: Enter the cookbook description here.
+
---
+++ lamp_demo/metadata.rb
@@ -0,0 +1,8 @@
+name             'lamp_demo'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures lamp_demo'
+long_description 'Installs/Configures lamp_demo'
+version          '0.1.1'
+
